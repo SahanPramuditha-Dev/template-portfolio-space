8bcd3cc1f68167279286a82bae1d7f1adf0a6e8e
Sahan Pramuditha <sahan.dev.tech@gmail.com>
2026-08-24 15:02:10 +0530

    Update favicon to round brand icon

--- src/components/GlobalHelmet.jsx
+++ src/components/GlobalHelmet.jsx
@@ -11,13 +11,16 @@ const GlobalHelmet = () => {
     <Helmet>
       <title>{siteDoc.seoTitle || 'Sahan Pramuditha'}</title>
       <meta name="description" content={siteDoc.seoDescription || ''} />
-      {siteDoc.seoFavicon && (
-        <link 
-          rel="icon" 
-          href={siteDoc.seoFavicon} 
-          sizes="any" 
-        />
-      )}
+      <link
+        rel="icon"
+        href="/favicon.png?v=2026-08-24"
+        sizes="any"
+      />
+      <link
+        rel="shortcut icon"
+        href="/favicon.ico?v=2026-08-24"
+        type="image/x-icon"
+      />
       {siteDoc.seoImage && (
         <meta property="og:image" content={siteDoc.seoImage} />
       )}
